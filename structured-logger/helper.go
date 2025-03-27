8d501e1c8d52cae4ce099fd57f5bbbbaedb3e9e2
y-du <42994541+y-du@users.noreply.github.com>
2025-03-27 12:15:11 +0100

    add convenience function and config

--- structured-logger/helper.go
+++ structured-logger/helper.go
@@ -17,6 +17,7 @@
 package structured_logger
 
 import (
+	"github.com/SENERGY-Platform/go-service-base/structured-logger/attributes"
 	"io"
 	"log/slog"
 	"os"
@@ -65,4 +66,35 @@ func GetHandler(value string, writer io.Writer, opts *slog.HandlerOptions, defau
 
 func GetLogFile(filePath string, filePerm os.FileMode) (io.WriteCloser, error) {
 	return os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, filePerm)
+}
+
+type Config struct {
+	Handler    string `json:"handler" env_var:"LOGGER_HANDLER"`
+	Level      string `json:"level" env_var:"LOGGER_LEVEL"`
+	TimeFormat string `json:"time_format" env_var:"LOGGER_TIME_FORMAT"`
+	TimeUtc    bool   `json:"time_utc" env_var:"LOGGER_TIME_UTC"`
+	FilePath   string `json:"file_path" env_var:"LOGGER_FILE_PATH"`
+	AddSource  bool   `json:"add_source" env_var:"LOGGER_ADD_SOURCE"`
+	AddMeta    bool   `json:"add_meta" env_var:"LOGGER_ADD_META"`
+}
+
+func New(c Config, out io.Writer, organization, project string) *slog.Logger {
+	recordTime := NewRecordTime(c.TimeFormat, c.TimeUtc)
+	options := &slog.HandlerOptions{
+		AddSource:   c.AddSource,
+		Level:       GetLevel(c.Level, slog.LevelInfo),
+		ReplaceAttr: recordTime.ReplaceAttr,
+	}
+	handler := GetHandler(c.Handler, out, options, slog.Default().Handler())
+	if c.AddMeta {
+		var attr []slog.Attr
+		if organization != "" {
+			attr = append(attr, slog.String(attributes.OrganizationKey, organization))
+		}
+		if project != "" {
+			attr = append(attr, slog.String(attributes.ProjectKey, project))
+		}
+		handler = handler.WithAttrs(attr)
+	}
+	return slog.New(handler)
 }
